d4673e84141de6ea8d4d7e0f5bfbc99bb47ec4d0
Alishaan Asaria <alishaan.asaria@gmail.com>
2018-03-02 19:20:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'alishaanasaria',
+    database: 'alishaanasaria',
+    password: process.env.DB_PASSWORD,
+    host: 'db.imad.hasura-app.io',
+    port: '5432'
+}
+
 
 var app = express();
 app.use(morgan('combined'));
@@ -65,6 +75,16 @@ function createTemplate(data) {
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+var pool = new Pool(config);
+app.get('/test-db', function (req,res) {
+    pool.query('SELECT * FROM article', function (err, result) {
+        if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            res,send(JSON.stringify(result));
+        }
+    })
 });
 
 var counter = 0;
